01ee52555b647802050a413a945f8286a12e2364
LaNice Powell <lanicemp@aol.com>
2019-07-03 23:54:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-end
+
 return programmer_hash[:dennis_ritchie][:languages][0]
+end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
